76f579d6d7a6c6c5bce0b233abbf2a13297ffd96
mnl150430 <44811930+mnl150430@users.noreply.github.com>
2018-11-26 11:35:47 -0600

    King should have legal movements

--- Assets/Scripts/Pieces/King.cs
+++ Assets/Scripts/Pieces/King.cs
@@ -6,6 +6,17 @@ public class King : Piece
     public override List<Vector2Int> MoveLocations(Vector2Int gridPoint)
     {
         List<Vector2Int> locations = new List<Vector2Int>();
+        
+        locations.Add(new Vector2Int(gridPoint.x - 1, gridPoint.y - 1));
+        locations.Add(new Vector2Int(gridPoint.x + 1, gridPoint.y - 1));
+        locations.Add(new Vector2Int(gridPoint.x - 1, gridPoint.y + 1));
+        locations.Add(new Vector2Int(gridPoint.x + 1, gridPoint.y + 1));
+
+        locations.Add(new Vector2Int(gridPoint.x, gridPoint.y - 1));
+        locations.Add(new Vector2Int(gridPoint.x, gridPoint.y + 1));
+        locations.Add(new Vector2Int(gridPoint.x + 1, gridPoint.y));
+        locations.Add(new Vector2Int(gridPoint.x - 1, gridPoint.y));
+        
         return locations;
     }
 }
